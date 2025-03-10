1c5c3c72ce1c42cc9d194827e6434b17d490f16a
Cephas <cephaskiprono@gmail.com>
2025-03-11 01:15:55 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
           agriculture. Can you be part of us?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to AgriIS today and let&apos;s share in the vision.
+          Reach out to Agri I S today and let&apos;s share in the vision.
         </p>
 
         {/* <a href="mailto:contact@jsmastery.pro">
@@ -37,18 +37,18 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 AgriIS
+          Copyright © 2024 Agri I S
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
+          {/* {socialMedia.map((info) => (
             <div
               key={info.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
     </footer>
